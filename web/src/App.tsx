bd1706751d617d365262c53126afc94c98a2509c
Bruno Bach <42494225+brunobach@users.noreply.github.com>
2020-08-03 21:58:11 -0300

    Add Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './assets/styles/global.css'
+
+import Landing from './pages/Landing'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Bruno</h1>
-    </div>
+   <Landing />
   );
 }
 
